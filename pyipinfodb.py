f5d3e4af2e9677952e2fb6450f9be98ad4bd95c8
Westly Ward <sonicrules1234@gmail.com>
2010-11-16 08:35:22 -0800

    Fixing the module to work with the v2 API.  You will need to get an API key from http://ipinfodb.com

--- pyipinfodb.py
+++ pyipinfodb.py
@@ -26,33 +26,35 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import json, urllib, urllib2, socket
+class IPInfo() :
+    def __init__(self, apikey) :
+        self.apikey = apikey
+    def GetIPInfo(self, baseurl, ip=None, timezone=False) :
+        """Same as GetCity and GetCountry, but a baseurl is required.  This is for if you want to use a different server that uses the the php scripts on ipinfodb.com."""
+        passdict = {"output":"json", "key":self.apikey}
+        if ip :
+            try :
+                passdict["ip"] = socket.gethostbyaddr(ip)[2][0]
+            except : passdict["ip"] = ip
+        if timezone :
+            passdict["timezone"] = "true"
+        else :
+            passdict["timezone"] = "false"
+        urldata = urllib.urlencode(passdict)
+        url = baseurl + "?" + urldata
+        urlobj = urllib2.urlopen(url)
+        data = urlobj.read()
+        urlobj.close()
+        datadict = json.loads(data)
+        return datadict
 
-def GetIPInfo(baseurl, ip=None, timezone=False) :
-    """Same as GetCity and GetCountry, but a baseurl is required.  This is for if you want to use a different server that uses the the php scripts on ipinfodb.com."""
-    passdict = {"output":"json"}
-    if ip :
-        try :
-            passdict["ip"] = socket.gethostbyaddr(ip)[2][0]
-        except : passdict["ip"] = ip
-    if timezone :
-        passdict["timezone"] = "true"
-    else :
-        passdict["timezone"] = "false"
-    urldata = urllib.urlencode(passdict)
-    url = baseurl + "?" + urldata
-    urlobj = urllib2.urlopen(url)
-    data = urlobj.read()
-    urlobj.close()
-    datadict = json.loads(data)
-    return datadict
+    def GetCity(self, ip=None, timezone=False) :
+        """Gets the location with the context of the city of the given IP.  If no IP is given, then the location of the client is given.  The timezone option defaults to False, to spare the server some queries."""
+        baseurl = "http://api.ipinfodb.com/v2/ip_query.php"
+        return self.GetIPInfo(baseurl, ip, timezone)
 
-def GetCity(ip=None, timezone=False) :
-    """Gets the location with the context of the city of the given IP.  If no IP is given, then the location of the client is given.  The timezone option defaults to False, to spare the server some queries."""
-    baseurl = "http://ipinfodb.com/ip_query.php"
-    return GetIPInfo(baseurl, ip, timezone)
-
-def GetCountry(ip=None, timezone=False) :
-    """Gets the location with the context of the country of the given IP.  If no IP is given, then the location of the client is given.  The timezone option defaults to False, to spare the server some queries."""
-    baseurl = "http://ipinfodb.com/ip_query_country.php"
-    return GetIPInfo(baseurl, ip, timezone)
+    def GetCountry(self, ip=None, timezone=False) :
+        """Gets the location with the context of the country of the given IP.  If no IP is given, then the location of the client is given.  The timezone option defaults to False, to spare the server some queries."""
+        baseurl = "http://api.ipinfodb.com/v2/ip_query_country.php"
+        return self.GetIPInfo(baseurl, ip, timezone)
 
