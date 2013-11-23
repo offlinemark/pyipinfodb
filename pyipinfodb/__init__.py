cc790aca6d91ab224d58dbbcdccb7a00e318f536
Mark Mossberg <mark.mossberg@gmail.com>
2013-11-23 17:52:11 -0500

    add python package stuff
    - add setup.py, and directory structure containing __init__.py, and the
      script

---
+++ pyipinfodb/__init__.py
@@ -0,0 +1,1 @@
+from pyipinfodb import *
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='pyipinfodb',
+        version='0.1',
+        description='Simple wrapper for IPInfoDB API',
+        url='http://github.com/markmossberg/pyipinfodb',
+        author='Mark Mossberg',
+        author_email='mark.mossberg@gmail.com',
+        license='WTFPL',
+        packages=find_packages()
+)
