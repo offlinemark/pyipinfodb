cb50a869d93aa8fdd99d6fad9fa6c5c67866bdbd
Mark Mossberg <mark.mossberg@gmail.com>
2015-04-01 09:51:27 -0400

    Update tests
    I think ipinfodb changed something in their back end

--- tests/pyipinfodb_tests.py
+++ tests/pyipinfodb_tests.py
@@ -7,7 +7,7 @@ i = pyipinfodb.IPInfo(apikey)
 
 def test_country():
     ideal = {
-        u'countryName': u'UNITED STATES',
+        u'countryName': u'United States',
         u'ipAddress': u'155.33.148.202',
         u'countryCode': u'US',
         u'statusMessage': u'',
@@ -17,16 +17,16 @@ def test_country():
     assert_equal(ideal, test)
 
 def test_city():
-    ideal = {u'cityName': u'MOUNTAIN VIEW',
+    ideal = {u'cityName': u'Mountain View',
          u'countryCode': u'US',
-         u'countryName': u'UNITED STATES',
+         u'countryName': u'United States',
          u'ipAddress': u'8.8.8.8',
          u'latitude': u'37.406',
          u'longitude': u'-122.079',
-         u'regionName': u'CALIFORNIA',
+         u'regionName': u'California',
          u'statusCode': u'OK',
          u'statusMessage': u'',
-         u'timeZone': u'-08:00',
+         u'timeZone': u'-07:00',
          u'zipCode': u'94043'
     }
     test = i.get_city('8.8.8.8')
